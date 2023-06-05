85cc3d0f242d122e9118cdc6d9610357522c572d
amineux <29575893+amineux@users.noreply.github.com>
2023-06-05 10:00:08 +0200

    Update cleandata.py

--- cleandata.py
+++ cleandata.py
@@ -1,54 +1,23 @@
 import pandas as pd
-import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 
-def data_cleaning_tool(data):
-    # Handling missing values
-    data = handle_missing_values(data)
+# Load the movie ratings dataset
+ratings = pd.read_csv('ratings.csv')
 
-    # Removing duplicates
-    data = remove_duplicates(data)
+# Compute item-item similarity matrix
+item_similarity = cosine_similarity(ratings.T)
 
-    # Standardizing data formats
-    data = standardize_data_formats(data)
+# Function to get movie recommendations based on user input
+def get_movie_recommendations(movie_title, top_n=5):
+    movie_index = ratings.columns.get_loc(movie_title)
+    similar_movies = list(enumerate(item_similarity[movie_index]))
+    similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)
+    top_similar_movies = [ratings.columns[i[0]] for i in similar_movies[1:top_n+1]]
+    return top_similar_movies
 
-    # Perform additional data cleaning tasks
-  
+# Get movie recommendations for a given movie
+recommendations = get_movie_recommendations('Toy Story')
 
-    return data
+# Print the recommended movies
+print(recommendations)
 
-def handle_missing_values(data):
-    # Drop rows with missing values
-    data = data.dropna()
-
-    # Fill missing values with appropriate techniques
-    # data = data.fillna(value) or data = data.interpolate()
-
-    return data
-
-def remove_duplicates(data):
-    # Drop duplicate rows
-    data = data.drop_duplicates()
-
-    return data
-
-def standardize_data_formats(data):
-    # Convert 'Date' column to datetime format
-    data['Date'] = pd.to_datetime(data['Date'])
-
-    # Remove unwanted symbols or characters from specific columns
-    data['Amount'] = data['Amount'].str.replace('$', '')
-
-    # Perform additional data format standardization
-   
-
-    return data
-
-# Example usage
-# Load the data from a CSV file
-data = pd.read_csv('data.csv')
-
-# Clean the data using the Data Cleaning Tool
-cleaned_data = data_cleaning_tool(data)
-
-# Display the cleaned data
-print(cleaned_data.head())
